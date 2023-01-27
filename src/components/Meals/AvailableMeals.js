6901d74ddd1ceef6f0fd5c353d6914ba04caa5e8
Kaustubh Singh <73029422+kaustubsingh09@users.noreply.github.com>
2023-01-27 13:39:48 +0530

    Update AvailableMeals.js

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,10 +4,6 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
 const DUMMY_MEALS = [
   {
     id: 'm1',
